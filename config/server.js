54f174e1cefcd7a6863f0f9d996ab57669e94e2f
mackenziewritescode <mackenzie.charlton@gmail.com>
2020-09-25 16:51:19 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,6 @@
+module.exports = {
+  APP_ACCESS_KEY:
+    process.env.APP_ACCESS_KEY || "NUrur4YTq1nmDOTVbAlt4YuFh8eJ2rE6KZem2x-3x9I",
+  SECRET: process.env.SECRET || "muWWdMoWVIz4h-2Y03k8sopLjlF8IhN-PwMi6hG9sfg",
+  CALLBACK_ID: process.env.CALLBACK_ID || "http://localhost:3000",
+};
