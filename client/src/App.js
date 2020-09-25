747b49f0681174adb877eb646c516711da492f18
mackenziewritescode <mackenzie.charlton@gmail.com>
2020-09-25 17:12:32 -0400

    adding react

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <input type="text"></input>
+      <div>photo</div>
+      <div>photo</div>
+      <div>photo</div>
+      <div>photo</div>
+      <div>loading...</div>
+      <div>error</div>
+    </div>
+  );
+}
+
+export default App;
