b956d57bba18b5acfd98266620452446d6bb3dcb
mackenziewritescode <mackenzie.charlton@gmail.com>
2020-10-03 11:21:05 -0400

    tidying code

--- config/server.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  APP_ACCESS_KEY:
-    process.env.APP_ACCESS_KEY || "NUrur4YTq1nmDOTVbAlt4YuFh8eJ2rE6KZem2x-3x9I",
-  SECRET: process.env.SECRET || "muWWdMoWVIz4h-2Y03k8sopLjlF8IhN-PwMi6hG9sfg",
-  CALLBACK_ID: process.env.CALLBACK_ID || "http://localhost:3000",
-};
